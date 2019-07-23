b671692c0ee018855aabb490188f323da9e56abb
Chase Mitchusson <cmtchssn@gmail.com>
2019-07-23 12:41:49 -0500

    Procedural Cube

---
+++ Dissertation_VR/Assets/Scripts/ProceduralTextureScripts/ProceduralCube.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof(MeshFilter), typeof(MeshRenderer))]
+public class ProceduralCube : MonoBehaviour
+{
+    Mesh mesh;
+    List<Vector3> vertices;
+    List<int> triangles;
+
+    private void Awake()
+    {
+        mesh = GetComponent<MeshFilter>().mesh;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MakeCube();
+        UpdateMesh();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void MakeCube()
+    {
+        vertices = new List<Vector3>();
+        triangles = new List<int>();
+
+        for (int i = 0; i < 6; i++)
+        {
+            MakeFace(i);
+        }
+    }
+
+    void MakeFace(int dir)
+    {
+        vertices.AddRange(CubeMeshData.faceVertices(dir));
+        int vCount = vertices.Count;
+
+        triangles.Add(vCount - 4);
+        triangles.Add(vCount - 4 + 1);
+        triangles.Add(vCount - 4 + 2);
+        triangles.Add(vCount - 4);
+        triangles.Add(vCount - 4 + 2);
+        triangles.Add(vCount - 4 + 3);
+
+
+    }
+
+    void UpdateMesh()
+    {
+        mesh.Clear();
+
+        mesh.vertices = vertices.ToArray();
+        mesh.triangles = triangles.ToArray();
+        mesh.RecalculateNormals();
+    }
+}
