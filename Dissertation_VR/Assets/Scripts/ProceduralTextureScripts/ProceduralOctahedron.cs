0857bd60dbc14ed68b66a2bdbc42a0174d1862c9
Chase Mitchusson <cmtchssn@gmail.com>
2019-07-24 21:55:16 -0500

    octahedron generation, collision issues persist

--- Dissertation_VR/Assets/Scripts/ProceduralTextureScripts/ProceduralOctahedron.cs
+++ Dissertation_VR/Assets/Scripts/ProceduralTextureScripts/ProceduralOctahedron.cs
@@ -2,34 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent (typeof(MeshFilter), typeof(MeshRenderer), typeof(MeshCollider))]
-public class ProceduralCube : MonoBehaviour
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class ProceduralOctahedron : MonoBehaviour
 {
     Mesh mesh;
-    MeshCollider meshCollider;
     List<Vector3> vertices;
     List<int> triangles;
 
     private void Awake()
     {
         mesh = GetComponent<MeshFilter>().mesh;
-        meshCollider = GetComponent<MeshCollider>();
-        meshCollider.sharedMesh = mesh;
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        MakeCube();
+        MakeOctahedron();
         UpdateMesh();
     }
 
-    void MakeCube()
+    void MakeOctahedron()
     {
         vertices = new List<Vector3>();
         triangles = new List<int>();
 
-        for (int i = 0; i < 6; i++)
+        for (int i = 0; i < 8; i++) //8 sides
         {
             MakeFace(i);
         }
@@ -37,17 +34,12 @@ public class ProceduralCube : MonoBehaviour
 
     void MakeFace(int dir)
     {
-        vertices.AddRange(CubeMeshData.faceVertices(dir));
+        vertices.AddRange(OctahedronMeshData.faceVertices(dir));
         int vCount = vertices.Count;
 
-        triangles.Add(vCount - 4);
-        triangles.Add(vCount - 4 + 1);
-        triangles.Add(vCount - 4 + 2);
-        triangles.Add(vCount - 4);
-        triangles.Add(vCount - 4 + 2);
-        triangles.Add(vCount - 4 + 3);
-
-
+        triangles.Add(vCount - 3);
+        triangles.Add(vCount - 3 + 1);
+        triangles.Add(vCount - 3 + 2);
     }
 
     void UpdateMesh()
